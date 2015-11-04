0d13fe10458717a229a77406f15b77a9305591e4
tsumina <f0230074@gmail.com>
2015-11-04 21:20:03 +0900

    # TODO: since there is a data overflow when temperature from 5849 to 5850

--- cvm/naturalIteMethod/nim_tetra.py
+++ cvm/naturalIteMethod/nim_tetra.py
@@ -39,7 +39,7 @@ class process(object):
         #######################
         # init
         ########################
-        self.mu[0] = -0.08
+        self.mu[0] = -0.00
         self.mu[1] = -self.mu[0]
 
         self.x_i[0] = data.x_1
@@ -49,11 +49,11 @@ class process(object):
         self.y_ij[1, 1] = self.x_i[1] - self.y_ij[1, 0]
 
         self.e_ijkl[1, 0, 0, 0] = \
-            -(6 * data.int_pair + 8 * data.int_trip + 2 * data.int_tetra)
+            -(6 * data.int_pair + 8 * data.int_trip + 2 * data.int_tetra) / 4
         self.e_ijkl[1, 1, 0, 0] = \
-            -(12 * data.int_pair + 24 * data.int_trip + 6 * data.int_tetra)
+            -(12 * data.int_pair + 24 * data.int_trip + 6 * data.int_tetra) / 6
         self.e_ijkl[1, 1, 1, 0] = \
-            -(6 * data.int_pair + 16 * data.int_trip + 6 * data.int_tetra)
+            -(6 * data.int_pair + 16 * data.int_trip + 6 * data.int_tetra) / 4
 
         # run
         self.__run()
@@ -62,34 +62,44 @@ class process(object):
 
     def __eta_ijkl(self, i, j, k, l):
         """
-        η_ijkl = exp[-β*e_ijkl + (β/8)(mu + mu_j + mu_k + mu_l)]
+        η_ijkl = exp[-β*e_ijkl + (β/8)(mu_i + mu_j + mu_k + mu_l)]
                     * X^(-5/8)
                     * Y^(1/2)
         X = x_i * x_j * x_k * x_l
         Y = y_ij * y_ik * y_il * y_jk * y_jl * y_kl
         """
-        eta_ijkl = np.exp(-self.beta * self.e_ijkl[i, j, k, l] +
-                          (self.beta / 8) *
-                          (self.mu[i] + self.mu[j] +
-                           self.mu[k] + self.mu[l]))
-        X = np.power(
-            (self.x_i[i] * self.x_i[j] * self.x_i[k] * self.x_i[l]), -5 / 8)
-        Y = np.power((self.y_ij[i, j] * self.y_ij[i, k] * self.y_ij[i, l]
-                      * self.y_ij[j, k] * self.y_ij[j, l] * self.y_ij[k, l]),
-                     1 / 2)
-        return eta_ijkl * X * Y
+        # exp
+        exp = np.exp(-self.beta * self.e_ijkl[i, j, k, l] +
+                     (self.beta / 8) *
+                     (self.mu[i] + self.mu[j] +
+                      self.mu[k] + self.mu[l]))
+
+        # X
+        X = self.x_i[i] * self.x_i[j] * self.x_i[k] * self.x_i[l]
+
+        # Y
+        Y = self.y_ij[i, j] * self.y_ij[i, k] * self.y_ij[i, l] * \
+            self.y_ij[j, k] * self.y_ij[l, j] * self.y_ij[k, l]
+
+        return exp * np.power(X, -5 / 8) * np.power(Y, 1 / 2)
 
     def __z_ijkl(self):
         """
         y_ij = η_ij * exp(β*λ/2)
         """
         eta_1111 = self.__eta_ijkl(0, 0, 0, 0)
+        print('eta_1111 is: {}'.format(eta_1111))
         eta_2222 = self.__eta_ijkl(1, 1, 1, 1)
+        print('eta_2222 is: {}'.format(eta_2222))
         eta_2111 = self.__eta_ijkl(1, 0, 0, 0)
+        print('eta_2111 is: {}'.format(eta_2111))
         eta_2211 = self.__eta_ijkl(1, 1, 0, 0)
+        print('eta_2211 is: {}'.format(eta_2211))
         eta_2221 = self.__eta_ijkl(1, 1, 1, 0)
-        eta_sum = eta_2222 + eta_2221 + eta_2211 + eta_2111 + eta_1111
-        self.lam = -np.log(eta_sum) * 2 / self.beta
+        print('eta_2221 is: {}'.format(eta_2221))
+        eta_sum = eta_2222 + eta_2221 * 4 + \
+            eta_2211 * 6 + eta_2111 * 4 + eta_1111
+        self.lam = np.log(1 / eta_sum) * 2 / self.beta
         self.z_ijkl[0, 0, 0, 0] = eta_1111 * np.power(eta_sum, -1)
         self.z_ijkl[1, 1, 1, 1] = eta_2222 * np.power(eta_sum, -1)
         self.z_ijkl[1, 0, 0, 0] = eta_2111 * np.power(eta_sum, -1)
@@ -107,23 +117,23 @@ class process(object):
 
         # x_i
         self.x_i[0] = 1 * self.z_ijkl[0, 0, 0, 0] + \
-            3 / 4 * self.z_ijkl[1, 0, 0, 0] + \
-            3 / 6 * self.z_ijkl[1, 1, 0, 0] + \
-            1 / 4 * self.z_ijkl[1, 1, 1, 0]
+            3 * self.z_ijkl[1, 0, 0, 0] + \
+            3 * self.z_ijkl[1, 1, 0, 0] + \
+            1 * self.z_ijkl[1, 1, 1, 0]
         self.x_i[1] = 1 - self.x_i[0]
 
         # y_ij
         self.y_ij[0, 0] = 1 * self.z_ijkl[0, 0, 0, 0] + \
-            2 / 4 * self.z_ijkl[1, 0, 0, 0] + \
-            1 / 6 * self.z_ijkl[1, 1, 0, 0]
+            2 * self.z_ijkl[1, 0, 0, 0] + \
+            1 * self.z_ijkl[1, 1, 0, 0]
         self.y_ij[1, 0] = self.y_ij[0, 1] = 1 * self.z_ijkl[1, 0, 0, 0] + \
-            2 / 6 * self.z_ijkl[1, 1, 0, 0] + \
-            1 / 4 * self.z_ijkl[1, 1, 1, 0]
+            2 * self.z_ijkl[1, 1, 0, 0] + \
+            1 * self.z_ijkl[1, 1, 1, 0]
         self.y_ij[1, 1] = 1 * self.z_ijkl[1, 1, 0, 0] + \
-            2 / 4 * self.z_ijkl[1, 1, 1, 0] + \
+            2 * self.z_ijkl[1, 1, 1, 0] + \
             1 * self.z_ijkl[1, 1, 1, 1]
 
-        print(self.x_i)
+        print(self.y_ij)
         print('\n')
         if np.absolute(self.lam - lam) > 1e-6:
             self.__run()
