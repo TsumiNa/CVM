ddc5cfead0d6b8b1f96b92d26153f763110c4c91
tsumina <f0230074@gmail.com>
2017-04-19 18:50:01 +0900

    fix tetra-oct calculation error

--- cvm/A1/tetraOctahedron/tetraOctahedron.py
+++ cvm/A1/tetraOctahedron/tetraOctahedron.py
@@ -137,12 +137,6 @@ class tetraOctahedron(CVM):
                 # print(' mu:     {:06.4f}'.format(self.mu[0].item(0)))
                 # print(' 1st:    {:06.4f}'.format(int[0][0].item(0)))
                 # print(' 2nd:    {:06.4f}'.format(int[0][1].item(0)))
-                sample.res['1st'].append(int[0][0])
-                sample.res['2nd'].append(int[0][1])
-                sample.res['4th'].append(int[0][3])
-                sample.res['9th_a'].append(int[0][8])
-                sample.res['trip'].append(int[1])
-                sample.res['tetra'].append(int[2])
                 while self.checker > sample.condition:
                     while self.checker > self.main_condition:
                         process(self)
